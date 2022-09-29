ef70bbf1e4b76f5c55cf326f265c7b34c7bbe92c
Julien Bocage <julien.bocage@gmail.com>
2022-09-29 16:05:39 +0800

    Python script parser now copies the file's docstring

--- src/makedoc/parsers/pyscript_parser.py
+++ src/makedoc/parsers/pyscript_parser.py
@@ -1,4 +1,8 @@
-"""Implements a parser class for python scripts"""
+"""
+Implements a parser class for python scripts
+"""
+from typing import List
+
 from .concept import FileParserAbstract
 
 
@@ -15,4 +19,42 @@ class PyscriptParser(FileParserAbstract):
         """
         if self.parsed_doc == "":
             self.parsed_doc = f"# {self.name}\n"
+            self.parsed_doc += "\n\n".join(self.get_file_beginning_comment_lines())
         return self.parsed_doc
+
+    def get_file_beginning_comment_lines(self) -> List[str]:
+        """Gets the beginning of file docstrings if applicable
+
+        Returns
+            List[str] : The line-wise list of all comments of the beginning of
+                the file.
+        """
+        lines = self.content_txt_lines
+        file_beginning_comment_lines = []
+        comment_marker = None
+
+        for line in lines:
+
+            # If file beginning comment not started yet:
+            if comment_marker is None:
+
+                if line.lstrip()[:3] in ["'''", '"""']:  # Comment starts
+                    comment_marker = "'''" if line.lstrip()[:3] == "'''" else '"""'
+
+                    if (
+                        line.lstrip()[3:].rstrip()[-3:] == comment_marker
+                    ):  # One line comment
+                        return [line.strip()[3:-3]]
+                    elif line.rstrip()[3:]:
+                        file_beginning_comment_lines.append(line.strip()[3:])
+
+            else:
+                if line.lstrip()[:3] == comment_marker:
+                    return file_beginning_comment_lines
+                elif line.rstrip()[-3:] == comment_marker:
+                    file_beginning_comment_lines.append(line.rstrip()[:-3])
+                    return file_beginning_comment_lines
+                else:
+                    file_beginning_comment_lines.append(line.rstrip())
+
+        return []
